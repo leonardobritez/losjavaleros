f4a5bb1666435d03c5e4006c847f9a339a5733cd
leonardobritez <britez20008@gmail.com>
2021-12-19 14:46:05 -0300

    init

---
+++ src/main/java/javaleros/frba/javaleros/JavalerosApplication.java
@@ -0,0 +1,13 @@
+package javaleros.frba.javaleros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavalerosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavalerosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/javaleros/frba/javaleros/JavalerosApplicationTests.java
@@ -0,0 +1,13 @@
+package javaleros.frba.javaleros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavalerosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
