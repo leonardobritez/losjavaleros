719579d9832e2a9117780ef42819b51fffeeecfb
matias sarapura <matiasarapura@gmail.com>
2022-02-18 23:48:05 -0300

    fix

--- src/main/java/javaleros/frba/javaleros/security/SpringSecurityConfig.java
+++ src/main/java/javaleros/frba/javaleros/security/SpringSecurityConfig.java
@@ -80,6 +80,8 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 
             .antMatchers(HttpMethod.GET,"/mascota/{\\\\d+}").permitAll()
             .antMatchers("/mascota/**").authenticated()
+            .antMatchers("/mascota").authenticated()
+
 
             .antMatchers(HttpMethod.GET, "/hogaresDeTransito").permitAll()
 
